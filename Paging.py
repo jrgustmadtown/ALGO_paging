d2c2a360beea28136b6f1555f6aba5ec883c7c56
jrgustmadtown <jrgust@wisc.edu>
2025-10-01 17:41:20 -0500

    fix

--- Paging.py
+++ Paging.py
@@ -1,49 +1,48 @@
 import sys
 
-def cache(reqs, page, off):
-    i=0
-    for i in range(len(reqs)):
-        if reqs[i] == page:
-            return (page, i+off)
-        else:
-            i+=1
-
-
 def paging(ksize, n, reqs):
     faults = 0
-    i=0 #index of page
     k = []
 
-    if len(k) < k:
-        k.append(cache(reqs[i+1:], reqs[i], i))
-        faults+=1
-    else:
-        if reqs[i] not in k:
-            indices = list(map(lambda x: x[1], k))
-            far = indices.index(max(indices))
-            k[far] = cache(reqs[i+1:], reqs[i], i)
-                        
-            faults+=1
+    for i in range(n):
+        page = reqs[i]
+
+        if page in k:
+            continue
+
+        if len(k) < ksize:
+            k.append(page)
         else:
-            i+=1
-        
+            far = -1
+            remove = -1
+            follow = reqs[i+1:]
+            for h, p in enumerate(k):
+                if p in follow:
+                    next = follow.index(p)
+                else:
+                    next = float('inf')
+                if next > far:
+                    far = next
+                    remove = h
+            k[remove] = page
+            
+        faults+=1
 
     return faults
 
 inputy = sys.stdin.read().splitlines()
 t = int(inputy[0])
-index = 0
+index = 1
 outputy = []
 
 for _ in range(t):
-    index+=1
     ksize = int(inputy[index])
-    k = [0 for _ in range(ksize)]
     index+=1
     n = int(inputy[index])
     index+=1
     reqs = inputy[index]
-    outputy.append(paging(ksize, n, reqs))
+    index+=1
+    outputy.append(paging(ksize, n, list(reqs)))
 
 for _ in outputy:
     print(_)
