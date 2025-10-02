0f8219169fb9593cf08dee7d071ad0cf73362862
jrgustmadtown <jrgust@wisc.edu>
2025-10-01 20:10:33 -0500

    trying with heap

--- Paging.py
+++ Paging.py
@@ -1,39 +1,73 @@
 import sys
-
-def possers(reqs, n):
-    pos = [[] for _ in range(max(reqs)+1)]
-    for i in range(n - 1, -1, -1):
-        pos[reqs[i]].append(i)
-    return pos
+import heapq
 
 def paging(ksize, n, reqs):
-    faults = 0
-    k = []
-    pos = possers(reqs, n)
+    faults=0
+    k = set()
+    heap = [] #max heap
 
+    # find the earliest of each page
+    nexts = []      #requests and their index
+    for i, page in enumerate(reqs):
+        nexts.append((page, i))
+    nexts.sort() 
+    pagesorts = []
+    starts = []
+    curr = None
+
+    for i, (page, ind) in enumerate(nexts):
+        if page != curr:
+            pagesorts.append(page)
+            starts.append(i)
+            curr = page
+    pointers = [0] * len(pagesorts)
+    
+    #next use index (NUI) for any page - binary search
+    def NUI(page, index):
+        l = 0
+        h = len(pagesorts)-1
+        page_i = -1
+
+        while l<=h:
+            m = (l+h)//2
+            if pagesorts[m] == page:
+                page_i = m
+                break
+            elif pagesorts[m] < page:
+                l=m+1
+            else:
+                h=m-1
+        if page_i == -1:
+            return float('inf')
+        
+        start = starts[page_i]
+        ptr = pointers[page_i]
+
+        while start + ptr < len(nexts) and nexts[start+ptr][0] == page:
+            idx = nexts[start+ptr][1]
+            if idx > index:
+                pointers[page_i] = ptr
+                return index
+            ptr+=1
+        return float('inf')
+    
+    #page requests
     for i in range(n):
         page = reqs[i]
-        pos[page].pop()
-
         if page in k:
+            heapq.heappush(heap, (-NUI(page, i), page))
             continue
-
-        if len(k) < ksize:
-            k.append(page)
-        else:
-            far = -1
-            remove = -1
-            for h, p in enumerate(k):
-                if pos[p]:
-                    next = pos[p][-1]
-                else:
-                    next = float('inf')
-                if next > far:
-                    far = next
-                    remove = h
-            k[remove] = page
-            
         faults+=1
+
+        if len(k) >= ksize:
+            while heap:
+                _, p in heapq.heappop(heap)
+                if p in k:
+                    k.remove(p)
+                    break
+
+        k.add(page)
+        heapq.heappush(heap, (-NUI(page, i), page))
 
     return faults
 
